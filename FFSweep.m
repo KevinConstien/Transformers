bba81665ed3b1f0d79d6c2d4622818013f3f2cd1
KevinConstien <48533291+KevinConstien@users.noreply.github.com>
2019-03-14 16:28:51 -0500

    Update FFSweep.m

--- FFSweep.m
+++ FFSweep.m
@@ -12,13 +12,16 @@ for i = 1:20:length(unique(coTable.FreqHz))
     freq = coTable.FreqHz(i);
     rows = (coTable.FreqHz == freq);
     r = coTable.y(rows);
-    S21 = coTable.S21DB(rows);
-    S21angle = coTable.S21DEG(rows);
+
+    S12 = coTable.S12DB(rows);
+    S21 = coTable.S21DB(rows);   
 
     figure()
-    plot(r - r(end)/2,S21);
+    plot(r - r(end)/2,S12,'-o');hold on;
+    plot(r - r(end)/2,S21,'-o');
 
     title("FF Horizontal Cut, Freq = " + freq/1e9+"GHz");
+    legend('S_1_2','S_2_2');
     ylabel('Magnitude (dB)');
     xlabel('Angle (\phi)');
 end
